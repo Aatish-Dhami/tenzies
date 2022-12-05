ac99b64a06deac0e34de57ee5b2e84876c3838a0
Aatish Dhami <aatishdhami@Aatishs-MBP.home>
2022-12-05 04:05:56 -0800

    Roll button added with functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
+import React from 'react';
 import Die from './components/Die';
 
 export default function App() {
+  
+  const [dice, setDice] = React.useState(allNewDice)
+
+  function allNewDice() { 
+    const arr = []
+    for(let i = 0; i < 10; i++){
+        arr.push(Math.floor(Math.random() * (6 - 1 + 1) + 1))            
+    }
+    return arr
+  } 
+
+  function rollDice() {
+    setDice(allNewDice)
+  }
+
+
+  const diceElement = dice.map((die) => {
+    return (
+      <Die value={die}/>
+    )
+  })
+  
   return (
     <main className='main'>
       <div className='dice-container'>
-        <Die value="1"/>
-        <Die value="2"/>
-        <Die value="3"/>
-        <Die value="4"/>
-        <Die value="5"/>
-        <Die value="6"/>
-        <Die value="1"/>
-        <Die value="3"/>
-        <Die value="4"/>
-        <Die value="6"/>
+        {diceElement}
       </div>
+      <button className='roll-btn' onClick={rollDice}>Roll</button>
     </main>
   );
 }
